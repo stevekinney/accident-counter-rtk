3cd58de629613eaa48c059800fbddb648621dddd
Steve Kinney <hello@stevekinney.net>
2022-11-18 11:39:35 -0600

    Add solution

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,23 @@
+import { useReducer } from 'react';
+import {
+  counterReducer,
+  decrement,
+  increment,
+  reducer,
+  reset,
+} from '../features/counter-reducer';
+
 const Counter = () => {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => dispatch(decrement(1))}>➖ Decrement</button>
+        <button onClick={() => dispatch(reset())}>🔁 Reset</button>
+        <button onClick={() => dispatch(increment(1))}>➕ Increment</button>
       </div>
       <div>
         <form onSubmit={(e) => e.preventDefault()}>
--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -2,24 +2,32 @@ import { createAction, createReducer } from '@reduxjs/toolkit';
 
 type CounterState = { count: number };
 
-const increment = createAction('INCREMENT', (amount: number) => {
+export const increment = createAction('INCREMENT', (amount: number) => {
   return { payload: amount };
 });
 
-const decrement = createAction('DECREMENT', (amount: number) => {
+export const decrement = createAction('DECREMENT', (amount: number) => {
   return { payload: amount };
 });
 
-const reset = createAction('RESET');
+export const reset = createAction('RESET');
 
 type CounterAction =
   | ReturnType<typeof increment>
   | ReturnType<typeof decrement>
   | ReturnType<typeof reset>;
 
-const counterReducer = createReducer({ count: 0 }, (builder) => {
+export const counterReducer = createReducer({ count: 0 }, (builder) => {
   builder.addCase(increment, (state, action) => {
     state.count += action.payload;
+  });
+
+  builder.addCase(decrement, (state, action) => {
+    state.count -= action.payload;
+  });
+
+  builder.addCase(reset, (state) => {
+    state.count = 0;
   });
 });
 
